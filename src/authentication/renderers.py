a2e5911dfaaafdad85b3b3f054a9d0ddf79e10da
Zaeem Maqsood <zaeem.maqsood@gmail.com>
2017-11-16 14:10:18 -0500

    Initial Commit

---
+++ src/authentication/renderers.py
@@ -0,0 +1,22 @@
+from core.renderers import ConduitJSONRenderer
+
+
+class UserJSONRenderer(ConduitJSONRenderer):
+	
+	object_label = 'user'
+
+	def render(self, data, media_type=None, renderer_context=None):
+
+		# If we receive a `token` key as part of the response, it will be a
+		# byte object. Byte objects don't serialize well, so we need to
+		# decode it before rendering the User object.
+		token = data.get('token', None)
+
+
+		if token is not None and isinstance(token, bytes):
+			# Also as mentioned above, we will decode `token` if it is of type
+			# bytes.
+			data['token'] = token.decode('utf-8')
+
+
+		return super(UserJSONRenderer, self).render(data)
